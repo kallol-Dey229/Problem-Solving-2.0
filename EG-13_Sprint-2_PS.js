944cb388038dec1a2f34c7c2e9e0843fccc68c1e
Kallol Dey <kalloldey067@gmail.com>
2026-08-29 10:22:54 +0600

    problem 2 solved

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -9,8 +9,31 @@ function reverseString(str) {
     for (let i = 1; i <= str.length; i++) {
         reversed.push(str[str.length - i])
     }
-    return reversed.join("")
+    return reversed.join("");
 
 }
 
-console.log(reverseString("hello"))
+// console.log(reverseString("hello"))
+
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function findMax(nums) {
+
+    let max = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+
+        if (nums[i] > max) {
+            max = nums[i]
+        }
+    }
+
+    return max;
+}
+
+// console.log(findMax([3, 7, 2, 9, 5]))
